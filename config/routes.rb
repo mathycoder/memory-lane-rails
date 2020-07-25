1ca87442f543332da83f6267fa8742878a03fc9c
Adam Sarli <adam.sarli@gmail.com>
2020-07-25 15:21:12 -0400

    quick commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get '*path', to: "static_pages#index", constraints: ->(request) do
+    !request.xhr? && request.format.html?
+  end
+
   resources :users, only: [:index]
 end
